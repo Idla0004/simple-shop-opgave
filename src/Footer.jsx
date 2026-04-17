1cf42ea17b6ef22177e084371b9f5b5e57ea6888
Kia <kivi0001@stud.kea.dk>
2026-04-17 14:14:49 +0200

    first footer syntax setup

---
+++ src/Footer.jsx
@@ -0,0 +1,36 @@
+const Footer = () => {
+  return (
+    <section className="grid grid-cols-1">
+      <div className="logo-and-icons">
+        <img src="" alt="" />
+        <div className="flex justify-between">
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="">
+        <h3>info</h3>
+        <p>about us</p>
+        <p>events</p>
+      </div>
+      <div>
+        <h3>help</h3>
+        <p>terms and conditions</p>
+        <p>privacy policy</p>
+      </div>
+      <div>
+        <h3>contact</h3>
+        <div className="flex">
+          <img src="" alt="" />
+          <p>+45 88 66 44 22</p>
+        </div>
+        <div className="flex">
+          <img src="" alt="" />
+          <p>koeb@email.com</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;

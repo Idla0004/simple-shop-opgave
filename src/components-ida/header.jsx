a872a9afc9dbc2f19dc8f823451b25201dfc07c8
Ida <idla0004@stud.kea.dk>
2026-04-17 14:24:45 +0200

    Billeder oprettet

---
+++ src/components-ida/header.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header>
+      <Image
+        src="/img/KOEBblue.png"
+        width={50}
+        height={50}
+      />
+    </header>
+  );
+};
+
+export default Header;

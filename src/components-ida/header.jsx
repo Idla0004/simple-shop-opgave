3e09c8d5da1248db1b33e72862e6935f8e0c41af
Ida <idla0004@stud.kea.dk>
2026-04-17 14:46:36 +0200

    tester

--- src/components-ida/header.jsx
+++ src/components-ida/header.jsx
@@ -2,12 +2,21 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header>
-      <Image
-        src="/img/KOEBblue.png"
-        width={50}
-        height={50}
-      />
+    <header className="px-2 mt-10 flex flex-row gap-1 bg-[#2D2D2D]">
+      <figure className="px-8">
+        <Image
+          src="/img/KOEBblue.png"
+          width={100}
+          height={30}
+          alt="Logo"
+          loading="eager"
+          className=""
+        />
+      </figure>
+      <ul className="flex gap-1">
+        <li>Home</li>
+        <li>Products</li>
+      </ul>
     </header>
   );
 };

be9c641f9b0178e3455b07157d31752a8e6abd80
Ida <idla0004@stud.kea.dk>
2026-04-20 10:10:39 +0200

    Opstart hihi

---
+++ src/components-ida/Hero.jsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <section>
+      <Image
+        src="https://placecats.com/750/750"
+        width={750}
+        height={750}
+        alt="Kitty cat"
+      />
+    </section>
+  );
+};
+
+export default Hero;

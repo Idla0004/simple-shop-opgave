981c037a460bb6bf0b26e1b44118276c7bb2e821
Kia <kivi0001@stud.kea.dk>
2026-04-20 13:02:52 +0200

    grid for productlist and global css fixed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -41,7 +41,7 @@ const ProductCard = () => {
         </div>
       </div>
       <div className="bg-(--color-grey-bg) py-4">
-        <h2 className="font-mulish font-normal mx-2 mb-4">
+        <h2 className="font-(--font-p-mulish) text-[28px] mx-2 mb-3">
           Product name
         </h2>
         <div className="grid grid-cols-2 gap-8">

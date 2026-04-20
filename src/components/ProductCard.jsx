679ba3567bdfc7d6196367528df25457f67619bb
Kia <kivi0001@stud.kea.dk>
2026-04-20 12:18:55 +0200

    card almost done

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,30 +1,56 @@
 import { FaRegHeart } from "react-icons/fa";
 import Image from "next/image";
+import "../app/local.css";
 
 const ProductCard = () => {
   return (
     <section className="max-w-97.5">
-      <div className="relative max-w-45">
-        <div className="flex justify-between absolute top-0 right-0 left-0">
-          <div className="bg-(--color-grey-bg)">
-            out of stock
+      <div className="relative">
+        <div className="flex justify-between absolute top-0 right-0 left-0 m-2 items-center">
+          <div className="text-(--color-light-font) text-[20px] px-2 py-0.5">
+            <p className="out-of-stock bg-(--color-grey-bg)">
+              out of stock
+            </p>
           </div>
-          <FaRegHeart></FaRegHeart>
+          <div className="rounded-full bg-(--color-background) p-2">
+            <FaRegHeart
+              size={30}
+              color="#630850"
+            ></FaRegHeart>
+          </div>
         </div>
         <Image
+          className="object-cover aspect-8/9"
           src="https://placecats.com/750/750"
-          width={390}
+          width={400}
           height={450}
           alt="Kitty cat"
-        ></Image>
+        />
+        <div className="flex justify-between absolute bottom-0 right-0 left-0">
+          <div className="bg-(--color-pink) px-5 self-start text-(--color-light-font) text-[24px]">
+            <p className="deal">DEAL</p>
+          </div>
+          <div>
+            <p className="on-sale-price text-[20px] px-4 text-end text-pink">
+              sale 234$
+            </p>
+            <p className="text-[20px] px-4 pb-4 text-end">
+              price 1234$
+            </p>
+          </div>
+        </div>
       </div>
-      <div>
-        <h2 className="font-mulish font-normal">
+      <div className="bg-(--color-grey-bg) py-4">
+        <h2 className="font-mulish font-normal mx-2 mb-4">
           Product name
         </h2>
-        <div>
-          <button>add to cart</button>
-          <button>buy now</button>
+        <div className="grid grid-cols-2 gap-8">
+          <button className="bg-(--color-foreground) py-1 px-2 text-(--color-light-font) text-[20px]">
+            ADD TO CART
+          </button>
+          <button className="bg-(--color-background) py-1 px-2 text-(--color-dark-font) text-[20px]">
+            BUY NOW
+          </button>
         </div>
       </div>
     </section>

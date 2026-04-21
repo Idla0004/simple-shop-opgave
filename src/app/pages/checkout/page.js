1e39a6cf328fcea113742b009ee3fc2789682b19
Ida <idla0004@stud.kea.dk>
2026-04-21 10:43:08 +0200

    Mere statisk styling

--- src/app/pages/checkout/page.js
+++ src/app/pages/checkout/page.js
@@ -16,20 +16,30 @@ export default function CheckOut() {
       </section>
       <section className="px-4 pt-8 grid col-2 col-end-2 h-fit min-w-full relative top-30 overflow-hidden">
         <CategoriesScroll />
-        <div className="px-10 pt-10 object-cover">
-          <div className="border-b flex justify-between w-125 pb-4">
-            <h2 className="row-start-1">
+        <div className="flex flex-col flex-wrap px-10 pt-10 object-cover">
+          <div className="border-b flex justify-between w-125 pb-4 border-dark-grey">
+            <h2 className="row-start-1 text-dark-grey">
               Shipping Fee
             </h2>
-            <p className="row-start-2">12.56,-</p>
+            <p className="row-start-2 text-dark-grey">
+              12.56,-
+            </p>
           </div>
           <div className="flex justify-between max-w-125 pt-6">
-            <h2 className="uppercase font-medium col-start-1">
+            <h2 className="uppercase font-medium col-start-1 text-dark-grey">
               Total
             </h2>
-            <p className="col-start-2">
+            <p className="col-start-2 text-dark-grey">
               1234.56,-
             </p>
+          </div>
+          <div className="pl-76">
+            <button
+              type="submit"
+              className="lowercase text-pink bg-blue rounded-md py-1 px-2 text-[20px] w-50 mt-8"
+            >
+              Check out
+            </button>
           </div>
         </div>
       </section>

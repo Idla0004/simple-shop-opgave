c95413f169335e323aa4d10d28fcda33fb696e1a
Ida <idla0004@stud.kea.dk>
2026-04-21 11:36:45 +0200

    Categories index gjort klar til dynamisk

--- src/app/pages/page.js
+++ src/app/pages/page.js
@@ -1,11 +1,18 @@
 import Hero from "@/components/Hero";
-import Categories from "@/components/CategoriesIndex";
+import CategoriesIndex from "@/components/CategoriesIndex";
 export default function Home() {
   return (
     <div className="bg-primary-light">
       <main className=" bg-primary-light">
         <Hero />
-        <Categories />
+        <section className="flex flex-col items-center justify-center w-screen">
+          <h2 className="uppercase text-2xl text-dark-font">
+            Shop by categories
+          </h2>
+          <ul className="w-screen flex gap-1 overflow-auto snap-x text-dark-font">
+            <CategoriesIndex />
+          </ul>
+        </section>
       </main>
     </div>
   );
--- src/components/CategoriesIndex.jsx
+++ src/components/CategoriesIndex.jsx
@@ -1,125 +1,18 @@
 import Image from "next/image";
 
-const Categories = () => {
+const CategoriesIndex = () => {
   return (
-    <section className="flex flex-col items-center justify-center w-screen">
-      <h2 className="uppercase text-2xl text-dark-font">
-        Shop by categories
-      </h2>
-      <ul className="w-screen flex gap-1 overflow-auto snap-x text-dark-font">
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>{" "}
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>{" "}
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>{" "}
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>{" "}
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>{" "}
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>{" "}
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>{" "}
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>{" "}
-        <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
-          <Image
-            src="https://placecats.com/350/350"
-            width={200}
-            height={200}
-            alt="cat"
-            className="rounded-full"
-          />
-          <p>Mens shirts</p>
-        </li>
-      </ul>
-    </section>
+    <li className="flex flex-col place-items-center basis-[min(250px,80%)] shrink-0 py-10 px-2 self-start snap-center snap-always">
+      <Image
+        src="https://placecats.com/350/350"
+        width={200}
+        height={200}
+        alt="cat"
+        className="rounded-full"
+      />
+      <p>Mens shirts</p>
+    </li>
   );
 };
 
-export default Categories;
+export default CategoriesIndex;

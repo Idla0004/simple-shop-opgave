b1c9f14967500f7df9ff2d810e60ad92ec1d4b2b
Kia <kivi0001@stud.kea.dk>
2026-04-22 13:45:24 +0200

    image gallery done

---
+++ src/components/Images.jsx
@@ -0,0 +1,65 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+const Images = ({
+  imagesource0,
+  imagesource1,
+  imagesource2,
+}) => {
+  const [bigImg, setBigImg] =
+    useState(imagesource0);
+
+  const updateBigImgSrc = (newSrc) => {
+    setBigImg(newSrc);
+  };
+
+  return (
+    <div className="flex m-4 flex-col gap-4 place-items-center mx-8">
+      <Image
+        className="object-cover aspect-square"
+        loading="eager"
+        src={bigImg}
+        width={700}
+        height={750}
+        alt="Kitty cat"
+      ></Image>
+      <div className="flex gap-4">
+        <Image
+          onClick={() =>
+            updateBigImgSrc(imagesource0)
+          }
+          className="object-cover max-w-[50%] aspect-6/7"
+          src={imagesource0}
+          width={400}
+          height={450}
+          alt="Kitty cat"
+        ></Image>
+        <div className="flex flex-col gap-4">
+          <Image
+            onClick={() =>
+              updateBigImgSrc(imagesource1)
+            }
+            className="object-cover aspect-5/4"
+            src={imagesource1}
+            width={400}
+            height={450}
+            alt="Kitty cat"
+          ></Image>
+          <Image
+            onClick={() =>
+              updateBigImgSrc(imagesource2)
+            }
+            className="object-cover aspect-5/4"
+            src={imagesource2}
+            width={400}
+            height={450}
+            alt="Kitty cat"
+          ></Image>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Images;

0dde551e8ab4bdcbe3454926d6e52c684f8c8906
Ida <idla0004@stud.kea.dk>
2026-04-22 13:08:21 +0200

    Sidebar navigation lavet til produktliste og filtrering af kategorier

--- src/app/(pages)/test/page.js
+++ src/app/(pages)/test/page.js
@@ -1,17 +1,25 @@
 // app/products/page.js
 import { Suspense } from "react";
 import ProductList from "@/components/ProductTest";
+import SideBarNavigation from "@/components/SiderBarNavigation";
 
 export default function ProductsPage({
   searchParams,
 }) {
   return (
-    <main>
-      <Suspense fallback={<div>Indlæser...</div>}>
-        <ProductList
-          searchParams={searchParams}
-        />
-      </Suspense>
-    </main>
+    <div className="flex items-start">
+      <SideBarNavigation />
+      <main className="flex-1 py-10 px-16 bg-[#E4DFD3]">
+        <div className="relative top-40 pb-25">
+          <Suspense
+            fallback={<div>Indlæser...</div>}
+          >
+            <ProductList
+              searchParams={searchParams}
+            />
+          </Suspense>
+        </div>
+      </main>
+    </div>
   );
 }
